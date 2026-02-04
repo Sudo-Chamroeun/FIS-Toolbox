82a07c6aad6fcde77b7d1463c6e6a45b8a3aea1d
Sudo-Chamroeun <157108313+Sudo-Chamroeun@users.noreply.github.com>
2026-02-04 12:38:14 +0700

    Update Browser-Controls.bat

--- Browser-Controls.bat
+++ Browser-Controls.bat
@@ -3,7 +3,7 @@ setlocal enabledelayedexpansion
 
 :menu
 cls
-mode con: cols=75 lines=15
+:: REMOVED THE MODE CON LINE TO FIX THE GLITCH
 title IT Department - FOOTPRINTS INTERNATIONAL SCHOOL
 
 echo.
@@ -12,57 +12,61 @@ echo   Browser Controls
 echo ====================
 echo.
 
-echo 1. Restrict Sign-in
-echo 2. Block Browsers
-echo 3. Reset Setting
+echo 1. Restrict Sign-in (Internal Domain Only)
+echo 2. Block All Browsers (Except Chrome)
+echo 3. Reset / Unblock All
 echo 4. Exit
 echo.
 
 set /p choice=Enter your choice: 
 
 if "%choice%"=="1" (
-    reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Google\Chrome" /v "BrowserSignin" /t REG_DWORD /d 2 /f
-    reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Google\Chrome" /v "RestrictSigninToPattern" /t REG_SZ /d "*@*t*t*o*.edu.kh" /f
+    echo Applying Sign-in Restrictions...
+    :: We use >nul 2>&1 to hide the "Operation Successful" text so it doesn't clutter the screen
+    reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Google\Chrome" /v "BrowserSignin" /t REG_DWORD /d 2 /f >nul 2>&1
+    reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Google\Chrome" /v "RestrictSigninToPattern" /t REG_SZ /d "*@*t*t*o*.edu.kh" /f >nul 2>&1
 
-    echo Restrict Sign-in pattern has been set to *@*t*t*o*.edu.kh domain.
+    echo [OK] Restrict Sign-in pattern set to *@*t*t*o*.edu.kh
     pause
     goto menu
 )
 
 if "%choice%"=="2" (
-    reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DisallowRun" /v "DisallowRun" /t REG_DWORD /d 1 /f
-    reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\firefox.exe" /v "Debugger" /t REG_SZ /d "C:\Windows\System32\taskkill.exe" /f
-    reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\iexplore.exe" /v "Debugger" /t REG_SZ /d "C:\Windows\System32\taskkill.exe" /f
-    reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\opera.exe" /v "Debugger" /t REG_SZ /d "C:\Windows\System32\taskkill.exe" /f
-    reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\safari.exe" /v "Debugger" /t REG_SZ /d "C:\Windows\System32\taskkill.exe" /f
-    reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\brave.exe" /v "Debugger" /t REG_SZ /d "C:\Windows\System32\taskkill.exe" /f
-    reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\msedge.exe" /v "Debugger" /t REG_SZ /d "C:\Windows\System32\taskkill.exe" /f
-    reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\launcher.exe" /v "Debugger" /t REG_SZ /d "C:\Windows\System32\taskkill.exe" /f
-    echo Browsers have been blocked. Only Chrome is allowed.
-    echo Press any key to continue...
-    pause >nul
+    echo Blocking Browsers...
+    reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DisallowRun" /v "DisallowRun" /t REG_DWORD /d 1 /f >nul 2>&1
+    
+    :: Redirecting all outputs to NULL to prevent "Ghost Text"
+    reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\firefox.exe" /v "Debugger" /t REG_SZ /d "C:\Windows\System32\taskkill.exe" /f >nul 2>&1
+    reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\iexplore.exe" /v "Debugger" /t REG_SZ /d "C:\Windows\System32\taskkill.exe" /f >nul 2>&1
+    reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\opera.exe" /v "Debugger" /t REG_SZ /d "C:\Windows\System32\taskkill.exe" /f >nul 2>&1
+    reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\safari.exe" /v "Debugger" /t REG_SZ /d "C:\Windows\System32\taskkill.exe" /f >nul 2>&1
+    reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\brave.exe" /v "Debugger" /t REG_SZ /d "C:\Windows\System32\taskkill.exe" /f >nul 2>&1
+    reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\msedge.exe" /v "Debugger" /t REG_SZ /d "C:\Windows\System32\taskkill.exe" /f >nul 2>&1
+    reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\launcher.exe" /v "Debugger" /t REG_SZ /d "C:\Windows\System32\taskkill.exe" /f >nul 2>&1
+    
+    echo [OK] Browsers have been blocked. Only Chrome is allowed.
+    pause
     goto menu
 )
 
 if "%choice%"=="3" (
-    reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Google\Chrome" /v "BrowserSignin" /f
-    reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Google\Chrome" /v "RestrictSigninToPattern" /f
-    reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\DisallowRun" /f
-    reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\firefox.exe" /f
-    reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\iexplore.exe" /f
-    reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\opera.exe" /f
-    reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\safari.exe" /f
-    reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\brave.exe" /f
-    reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\msedge.exe" /f
-    reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\launcher.exe" /f
+    echo Resetting configurations...
+    
+    :: The >nul 2>&1 here hides the "Error: Key not found" text which was causing your glitch!
+    reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Google\Chrome" /v "BrowserSignin" /f >nul 2>&1
+    reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Google\Chrome" /v "RestrictSigninToPattern" /f >nul 2>&1
+    
+    :: Cleaning up the blocked browsers
+    reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\firefox.exe" /f >nul 2>&1
+    reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\iexplore.exe" /f >nul 2>&1
+    reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\opera.exe" /f >nul 2>&1
+    reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\safari.exe" /f >nul 2>&1
+    reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\brave.exe" /f >nul 2>&1
+    reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\msedge.exe" /f >nul 2>&1
+    reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\launcher.exe" /f >nul 2>&1
 
-    rem Resetting option 1
-    reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Google\Chrome" /v "BrowserSignin" /t REG_DWORD /d 0 /f
-    reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Google\Chrome" /v "RestrictSigninToPattern" /f
-
-    echo All settings have been reset.
-    echo Press any key to continue...
-    pause >nul
+    echo [OK] All settings have been reset.
+    pause
     goto menu
 )
 
